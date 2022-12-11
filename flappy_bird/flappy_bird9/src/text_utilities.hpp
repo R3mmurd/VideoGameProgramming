a56c7716bd1c990120ed8ca7d473f3df6b77ea0c
Alejandro Mujica <alejandro.mujica@geniussports.com>
2022-12-10 19:03:19 -0500

    Removed old directories

--- flappy_bird/flappy_bird9/src/text_utilities.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-#include <SFML/Graphics.hpp>
-
-void center_text(sf::Text& text) noexcept;
-
-sf::Text build_text(const std::string& text_str, int size, const std::string& font_name, const sf::Color& color, bool center = false) noexcept;
