6783f6a1721ab5ed3f4b5dfdd299cc11a3ebbf30
Alejandro <aledrums@gmail.com>
2023-02-18 12:34:46 -0500

    Added scenes_tutorial

---
+++ scenes_tutorial/tutorial/src/scene_loader/__init__.py
@@ -0,0 +1,5 @@
+from src.scene_loader.TmxSceneLoader import TmxSceneLoader
+from src.scene_loader.JsonSceneLoader import JsonSceneLoader
+from src.scene_loader.YamlSceneLoader import YamlSceneLoader
+
+(TmxSceneLoader, JsonSceneLoader, YamlSceneLoader)
