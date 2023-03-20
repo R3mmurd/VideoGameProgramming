50282a4eb24789be5780ab05c4ba377650381404
Alejandro <aledrums@gmail.com>
2023-03-20 16:25:25 -0500

    Added DeadState

---
+++ 5.1-martian/martian6/src/states/entities/player_states/DeadState.py
@@ -0,0 +1,15 @@
+"""
+ISPPJ1 2023
+Study Case: Super Martian (Platformer)
+
+Author: Alejandro Mujica
+alejandro.j.mujic4@gmail.com
+
+This file contains the class DeadState for player.
+"""
+from src.states.entities.BaseEntityState import BaseEntityState
+
+
+class DeadState(BaseEntityState):
+    def enter(self) -> None:
+        self.entity.is_dead = True
