88888e5478612b36431079efefb1d39d394e2787
Alejandro Mujica <aledrums@gmail.com>
2024-10-19 12:38:26 -0400

    Some fixes

--- 08-throw_a_bird/throw_a_bird1/main/game_objects/parrot.go
+++ 08-throw_a_bird/throw_a_bird1/main/game_objects/parrot.go
@@ -1,28 +1,18 @@
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/main/atlas/characters.atlas\"\n"
-  "default_animation: \"parrot\"\n"
+  data: "default_animation: \"parrot\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/atlas/characters.atlas\"\n"
+  "}\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 embedded_components {
   id: "collisionobject"
   type: "collisionobject"
-  data: "collision_shape: \"\"\n"
-  "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
+  data: "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
   "mass: 2500.0\n"
   "friction: 1.0\n"
   "restitution: 0.1\n"
@@ -33,15 +23,8 @@ embedded_components {
   "  shapes {\n"
   "    shape_type: TYPE_SPHERE\n"
   "    position {\n"
-  "      x: 0.0\n"
-  "      y: 0.0\n"
-  "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
-  "      x: 0.0\n"
-  "      y: 0.0\n"
-  "      z: 0.0\n"
-  "      w: 1.0\n"
   "    }\n"
   "    index: 0\n"
   "    count: 1\n"
@@ -50,18 +33,5 @@ embedded_components {
   "}\n"
   "linear_damping: 0.1\n"
   "angular_damping: 0.7\n"
-  "locked_rotation: false\n"
-  "bullet: false\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
